71a69ec4e81eb66559249dfc433821e5c7d3fd59
ROGUE <88287526+Hackermanker@users.noreply.github.com>
2022-10-20 11:32:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton(' 𝗨𝗣𝗗𝗔𝗧𝗘𝗦♂️ ', url=f'https://t.me/botupdatesastra')
+                InlineKeyboardButton(' 𝗨𝗣𝗗𝗔𝗧𝗘𝗦♂️ ', url=f'https://t.me/ASTRA_Botz')
             ],
             [
                 InlineKeyboardButton(' 𝗛𝗘𝗟𝗣 ', url=f"https://t.me/{temp.U_NAME}?start=help")
@@ -45,7 +45,7 @@ async def start(client, message):
             InlineKeyboardButton(" 𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣𝗦 ", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton(" 𝗦𝗘𝗔𝗥𝗖𝗛 ", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton(" 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 ", url="https://t.me/botupdatesastra")
+            InlineKeyboardButton(" 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 ", url="https://t.me/ASTRA_Botz")
             ],[      
             InlineKeyboardButton(" 𝗛𝗘𝗟𝗣 ", callback_data="help"),
             InlineKeyboardButton(" 𝗔𝗕𝗢𝗨𝗧 ", callback_data="about")
@@ -90,7 +90,7 @@ async def start(client, message):
             InlineKeyboardButton(" 𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣𝗦 ", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton(" 𝗦𝗘𝗔𝗥𝗖𝗛 ", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton(" 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 ", url="https://t.me/botupdatesastra")
+            InlineKeyboardButton(" 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 ", url="https://t.me/ASTRA_Botz")
             ],[      
             InlineKeyboardButton(" 𝗛𝗘𝗟𝗣 ", callback_data="help"),
             InlineKeyboardButton(" 𝗔𝗕𝗢𝗨𝗧 ", callback_data="about")
